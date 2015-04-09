a498201e9ea1a460d9451341c336764e2c52f443
robsonoduarte <robson.o.d@gmail.com>
2015-04-08 23:22:33 -0300

    add default set to stories and  jpa anotation

--- mystudies-domain/src/main/java/br/com/mystudies/domain/entity/User.java
+++ mystudies-domain/src/main/java/br/com/mystudies/domain/entity/User.java
@@ -1,32 +1,21 @@
 package br.com.mystudies.domain.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="USER")
-public class User implements Serializable{
+public class User extends BaseEntity{
 
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="ID")
 	private Long id;
-
-	@Column(name="PASSWORD")
 	private String password;
-
-	@Column(name="EMAIL")
 	private String email;
-
 
 
 	public String getEmail() {
@@ -50,7 +39,6 @@ public class User implements Serializable{
 	public void setPassWord(String password) {
 		this.password = password;
 	}
-
 
 
 }
