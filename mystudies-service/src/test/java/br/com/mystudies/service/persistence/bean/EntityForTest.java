253e11cd9175049fa648645d29016863601b7520
Robson Duarte <robson.o.d@gmail.com>
2015-06-19 22:50:13 -0300

    add persistence lib of r3wa.

--- mystudies-service/src/test/java/br/com/mystudies/service/persistence/bean/EntityForTest.java
+++
@@ -1,5 +0,0 @@
-package br.com.mystudies.service.persistence.bean;
-
-import br.com.mystudies.domain.entity.Entity;
-
-public class EntityForTest implements Entity {private static final long serialVersionUID = 1L;}
